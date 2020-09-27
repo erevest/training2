935642519be12638c1a2f070245b52c67053dfa6
Андрей Раскалов <erevestroofs@hotmail.com>
2020-09-28 00:17:30 +0300

    12

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double v, t;
+	double a, rad;
+	int g = 10;
+	cout << "Введите v:=";
+	cin >> v;
+	cout << "Введите T:=";
+	cin >> t;
+	a = (asin((g * t) / (2 * v)));
+	cout << "Альфа равно: " << a << "\n";
+	return 0;
+}
